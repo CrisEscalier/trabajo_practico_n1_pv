ab403027059c11138b25f05d429cd0848c0b1e13
Cristian <Cristian@DESKTOP-PQFQ4PO>
2024-04-04 12:59:52 -0300

    Se agrega paquete y clase para el ejercicio1

---
+++ tp1_CristianEscalier/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,17 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int var1,var2,var3,var4,var5;
+		var1=9;
+		var2=21;
+		var3=15;
+		var4=4;
+		var5=3;
+		double promedio;
+		promedio = (var1+var2+var3+var4+var5)/5d;
+		System.out.println("El promedio es: " + promedio);
+	}
+
+}
